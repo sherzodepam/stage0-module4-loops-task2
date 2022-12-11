3ec9172202ac3dcc387dd94d3372c897bdfa31a3
Name Surname <StudentID@newuzbekistanuniversity.uz>
2022-12-11 22:38:35 +0500

    do smth

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,20 +2,20 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
-
-                int a = multiplyByAndToInclusive, begin = 0, end = 0;
-                if (a >= 0) {
-                    end = a;
-                } else {
-                    begin = a;
-
-                }
-
-                for (int i = begin; i <= end; ) {
-                    System.out.println(a * i);
-                    i = (begin == 0) ? i + 1 : i - 1;
-                }
-
+        int firstPrinted = 0;
+        int result = 0;
+        if (multiplyByAndToInclusive < 0) {
+            while (firstPrinted >= multiplyByAndToInclusive) {
+                result = (-1) * firstPrinted * multiplyByAndToInclusive;
+                System.out.println(result);
+                firstPrinted--;
+            }
+        } else if (multiplyByAndToInclusive > 0) {
+            while (firstPrinted <= multiplyByAndToInclusive) {
+                result = firstPrinted * multiplyByAndToInclusive;
+                System.out.println(result);
+                firstPrinted++;
             }
         }
-
+    }
+}
