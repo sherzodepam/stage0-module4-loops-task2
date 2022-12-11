77b602e0c11c86b7725902392b7c4177a0def168
Name Surname <StudentID@newuzbekistanuniversity.uz>
2022-12-11 18:53:19 +0500

    do smth

--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -4,8 +4,9 @@ public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
         int number=0;
         while(number<=lastPrinted){
-            System.out.println(number);
             number++;
+            System.out.println(number);
+
         }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,9 +2,17 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
-        for (int i=0; i<printToInclusive; i++){
-            System.out.println(i);
+        for (int i = 2; i <= printToInclusive; i++) {
+            boolean isPrime=false;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i%j==0){
+                    isPrime=true;
+                    break;
+                }
+            }
+            if (!isPrime){
+                System.out.println(i);
+            }
         }
-
     }
 }
